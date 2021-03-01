e2ea5dcd28c4620925fac88172a7454bb11d2feb
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2021-03-01 16:42:54 +0100

    Add data

---
+++ heidi-back/app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+import ulid
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_id():
+    arr = []
+    res = {}
+    res['unique_id'] = ulid.new().str
+    arr.append(res)
+    return json.dumps(arr)
+
+@app.route('/multiple/<int:nb>')
+def get_multiple_id(nb):
+    arr = []
+    try: 
+        if int(nb):
+            for i in range(nb):
+                res = {}
+                res['unique_id'] = ulid.new().str
+                arr.append(res)
+    except:
+            res = {}
+            res['unique_id'] = 'not a number'
+            arr.append(res)
+    return json.dumps(arr)
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port="4444")
+
